1ce009e54d8ba78343a928c41cdd0ed5141824e7
Rafael Sampaio Tavares <rafael_killua@msn.com>
2018-12-06 12:49:28 -0300

    SAC e Ajuda feitos

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,8 +1,16 @@
 <template>
     <v-footer app color="primary">
         <v-container fill-height>
+            <v-layout align-center justify-start>
+                <v-btn flat nuxt to="/sac" class="white--text">
+                    <strong>SAC</strong>
+                </v-btn>
+                <v-btn flat nuxt to="/ajuda" class="white--text">
+                    <strong>AJUDA</strong>
+                </v-btn>
+            </v-layout>
             <v-layout align-center justify-end>
-                <p class="white--text">
+                <span class="white--text">
                     &copy;2018 -
                     <a
                         href="https://rafaelst.com.br"
@@ -11,7 +19,7 @@
                     >
                         <strong>Rafael Sampaio Tavares</strong>
                     </a>
-                </p>
+                </span>
             </v-layout>
         </v-container>
     </v-footer>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container grid-list-md>
-        <v-layout align-center justify-center row wrap="">
+        <v-layout row wrap>
             <v-flex xs12>
-                <h1>^_^</h1>
+                
             </v-flex>
         </v-layout>
     </v-container>
